2b4e702dfe2aa5b5ba66860093ef9ca785fc9648
rohit-sanwariya-au7 <rohitsanwariya1995@gmail.com>
2020-03-24 11:54:31 +0530

    007

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const connectDB = require('./config/db');
+//connect db
+connectDB();
 app.get('/',(req,res,next)=>
 {
     res.send("api starts");
